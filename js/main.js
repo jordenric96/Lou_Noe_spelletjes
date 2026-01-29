6a5dd68025f6dc0ebf2284565eefda8ea85d36f2
jordenric96 <jordenricour96@gmail.com>
2026-01-29 14:01:11 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,54 +1,47 @@
-// Navigatie Logica
+// JS/MAIN.JS - Navigatie Logica
 
-// Schermen ophalen
 const mainMenu = document.getElementById('main-menu');
 const gameContainer = document.getElementById('game-container');
 const gameTitle = document.getElementById('game-title');
 const gameBoard = document.getElementById('game-board');
 
-// Functie om een spel te starten
+let currentGame = null; 
+
 function selectGame(gameName) {
-    // 1. Verberg hoofdmenu
     mainMenu.classList.remove('active');
     mainMenu.classList.add('hidden');
-
-    // 2. Toon spel container
     gameContainer.classList.remove('hidden');
     gameContainer.classList.add('active');
 
-    // 3. Zet de titel
-    let titleText = "";
-    
-    // 4. Start de logica van het specifieke spel
-    // (We roepen hier straks functies aan die in memory.js of doolhof.js staan)
+    currentGame = gameName; 
+
     if (gameName === 'memory') {
-        titleText = "Memory";
-        // Controleer of de memory functie al bestaat (voor later)
+        gameTitle.innerText = "Memory";
         if (typeof startMemorySetup === "function") {
             startMemorySetup(); 
-        } else {
-            gameBoard.innerHTML = "<p>Memory wordt geladen...</p>";
         }
     } else if (gameName === 'doolhof') {
-        titleText = "Doolhof";
-        gameBoard.innerHTML = "<p>Doolhof komt binnenkort!</p>";
-    } else {
-        titleText = "Spel";
+        gameTitle.innerText = "Doolhof";
+        // HIER ZIT DE UPDATE:
+        if (typeof startDoolhofSetup === "function") {
+            startDoolhofSetup();
+        } else {
+            gameBoard.innerHTML = "<p>Fout: doolhof.js is niet geladen!</p>";
+        }
+    } else if (gameName === 'patroon') {
+        gameTitle.innerText = "Patroon";
+        gameBoard.innerHTML = "<p>Patroon komt binnenkort!</p>";
     }
-
-    gameTitle.innerText = titleText;
 }
 
-// Functie om terug naar home te gaan
 function goHome() {
-    // 1. Verberg spel container
+    if (currentGame === 'doolhof' && typeof cleanupDoolhof === "function") {
+        cleanupDoolhof();
+    }
     gameContainer.classList.remove('active');
     gameContainer.classList.add('hidden');
-
-    // 2. Toon hoofdmenu
     mainMenu.classList.remove('hidden');
     mainMenu.classList.add('active');
-
-    // 3. Maak het spelbord leeg (zodat het reset)
     gameBoard.innerHTML = "";
+    currentGame = null;
 }
